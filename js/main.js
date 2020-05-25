ed3b4ae5db7c601729e81c821fb44fc0335b440e
SaeedMawed <saeed.moued@gmail.com>
2020-05-25 19:54:40 +0300

    rtl ltr

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
+var direction = document.getElementsByTagName("BODY")[0].getAttribute("dir");
+
 function openDashboard() {
     var dashboard = document.getElementById('dashboardSection');
     var open = document.getElementById('dashboard-open-btn');
     var close = document.getElementById('dashboard-close-btn');
-    dashboard.style.marginLeft = '0px';
+    if (direction === 'rtl') {
+        dashboard.style.marginRight = '0px';
+    } else {
+        dashboard.style.marginLeft = '0px';
+    }
+
     open.style.display = 'none';
     close.style.display = 'block';
 }
@@ -11,7 +18,11 @@ function closeDashboard() {
     var dashboard = document.getElementById('dashboardSection');
     var open = document.getElementById('dashboard-open-btn');
     var close = document.getElementById('dashboard-close-btn');
-    dashboard.style.marginLeft = '-350px';
+    if (direction == 'rtl') {
+        dashboard.style.marginRight = '-350px';
+    } else {
+        dashboard.style.marginLeft = '-350px';
+    }
     open.style.display = 'block';
     close.style.display = 'none';
 }
